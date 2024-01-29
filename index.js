ce1f39c8d01804f0294ed1bb3cc2f1af6656f3d8
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2024-01-29 11:00:43 +0100

    updates

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const { parse } = require('csv-parse')
-const fs = require('fs')
-const results = []
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const results = [];
+
+const isHabitablePlanet = (planet) => {
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
@@ -8,12 +15,14 @@ fs.createReadStream('kepler_data.csv')
         columns: true //returns each row in our csv file as a js object
     }))
     .on('data', (data)=>{
-        results.push(data)
+        if(isHabitablePlanet(data)){
+            results.push(data)
+        }
     })
     .on('error', (error)=>{
         console.log(error)
     })
     .on('end', ()=>{
-        console.log(results)
+        console.log(`${results} habitable planets found!`)
         console.log('done')
     })
